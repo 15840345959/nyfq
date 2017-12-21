27896a3938226840072a05b48f5a707b900dd555
ZhangMeng666666 <zm_happiness66@126.com>
2017-12-21 14:53:43 +0800

    by zm

--- app/Components/IndexManager.php
+++ app/Components/IndexManager.php
@@ -24,6 +24,10 @@ class IndexManager
     public static function getBannnerLists()
     {
         $banners = Banner::orderBy('sort','desc')->get();
-        return $banners;
+        if ($banners) {
+            return ApiResponse::makeResponse(true, $banners, ApiResponse::SUCCESS_CODE);
+        } else {
+            return ApiResponse::makeResponse(false, ApiResponse::$errorMassage[ApiResponse::NO_USER], ApiResponse::NO_USER);
+        }
     }
 }
